ac4af773d928e00f2c5cf6816ab6d1f464240281
zhangmingkai <zhangmingkai@cyou-inc.com>
2023-11-07 15:49:19 +0800

    清理代码

--- Exception.cs
+++ Exception.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GalgameNovelScript
 {
@@ -62,5 +58,5 @@ namespace GalgameNovelScript
         {
         }
     }
-   
+
 }
